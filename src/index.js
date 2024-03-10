4d804403dc1bfab419a884c7c8ac32ca98b457be
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-03-10 17:07:36 +0530

    Created utility files

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@ import connectDB from "./database/index.js";
 dotenv.config({
   path: "./.env",
 });
-connectDB();
+
+connectDB()
+  .then(() => {
+    const portNumber = process.env.PORT || 8000;
+    app.on("error", (error) => {
+      console.log(`Error : ${error}`);
+    });
+    app.listen(portNumber, () => {
+      console.log(`⚙ Server is running on http://localhost:${portNumber}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database connection error ${err.message}`);
+    process.exit(1);
+  });
