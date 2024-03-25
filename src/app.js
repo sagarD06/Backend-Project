09745a7c64a7c7464b9c6f11648a8c403dc8fd9c
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-03-25 20:15:11 +0530

    created comment controller

--- src/app.js
+++ src/app.js
@@ -21,10 +21,12 @@ app.use(cookieParser());
 import userRoute from "./routes/user.route.js";
 import tweetRoute from "./routes/tweet.route.js";
 import videoRoute from "./routes/video.route.js";
+import commentRoute from "./routes/comment.route.js";
 
 // Routes decleration
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/tweets", tweetRoute);
 app.use("/api/v1/videos", videoRoute);
+app.use("/api/v1/comments", commentRoute);
 
 export default app;
---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.models.js";
+import { ApiError } from "../utilities/ApiErrors.js";
+import { ApiResponse } from "../utilities/ApiResponse.js";
+import { asyncHandler } from "../utilities/asyncHandler.js";
+
+/***************************** GET ALL COMMENTS ****************************/
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+});
+
+/***************************** CREATE COMMENT ****************************/
+
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const user = req.user;
+  const { content } = req.body;
+
+  if (!videoId) throw new ApiError(400, "Video Id required!");
+  if (!user) throw new ApiError(400, "Invalid request, Please login!");
+  if (!content) throw new ApiError(400, "Content field is required!");
+
+  const newComment = await Comment.create({
+    content,
+    video: videoId,
+    owner: user?._id,
+  });
+
+  if (!newComment)
+    throw new ApiError(500, "Something went wrong while creating comment!");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newComment, "Comment created successfully!"));
+});
+
+/***************************** UPDATE COMMENT ****************************/
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const user = req.user;
+  const { content } = req.body;
+
+  if (!commentId) throw new ApiError(400, "Comment Id is required!");
+  if (!user) throw new ApiError(400, "Invalid request!");
+  if (!content) throw new ApiError(400, "Content field is required!");
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) throw new ApiError(404, "comment donot exist!");
+
+  if (user?._id.toString() !== comment.owner.toString())
+    throw new ApiError(400, "Not authorised to perform this action!");
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: { content },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedComment)
+    throw new ApiError(500, "Soething went wrong while updating the comment!");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updatedComment, "Comment Updated Successfully!")
+    );
+});
+
+/***************************** DELETE COMMENT ****************************/
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const user = req.user;
+
+  if (!commentId) throw new ApiError(400, "Comment Id is required!");
+  if (!user) throw new ApiError(400, "Invalid request!");
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) throw new ApiError(404, "comment donot exist!");
+
+  if (user?._id.toString() !== comment.owner.toString())
+    throw new ApiError(400, "Not authorised to perform this action!");
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+  if (!deletedComment)
+    throw new ApiError(500, "Soething went wrong while updating the comment!");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, deletedComment, "Comment Deleted Successfully!")
+    );
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
